fcbad7cf9945b3a9ef245609a5c697037e3eb5a6
Doug Rose <drrose@email.neit.edu>
2017-10-17 15:08:53 -0400

    adding dbconn

---
+++ lab2/assets/footer.php
@@ -0,0 +1,15 @@
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Doug
+ * Date: 10/16/2017
+ * Time: 12:02 PM
+ */
+?>
+</section>
+<footer>
+    Copyright &copy; <?php echo date("Y"); ?> Douglas Rose. All Rights Reserved.
+</footer>
+</body>
+</html>
