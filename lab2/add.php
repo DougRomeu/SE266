312518038da4f24ddec4d68873661f071cf6f6ea
Doug Rose <drrose@email.neit.edu>
2017-10-18 14:49:35 -0400

    moved add and view out of assets

--- lab2/add.php
+++ lab2/add.php
@@ -6,7 +6,7 @@
  * Time: 1:59 PM
  */
 
-include_once ("D:/xampp/htdocs/lab2/assets/header.php");
+include_once ("assets/header.php");
 include_once ("assets/addForm.php");
 
 include_once ("assets/footer.php");
