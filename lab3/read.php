384caad029b2eebdf30d188e9b4b3056de3a5ef3
Doug Rose <drrose@email.neit.edu>
2017-10-24 12:35:15 -0400

    add and read both working

--- lab3/read.php
+++ lab3/read.php
@@ -2,11 +2,14 @@
 <?php
 include_once ("assets/header.php");
 
-
 require_once ("assets/dbconn.php");
 require_once ("assets/corps.php");
 $db = dbconn();
 
 echo readCorp($db);
+?>
+<a href="index.php">Home</a>
+<?php
 include_once ("assets/footer.php");
 ?>
+
