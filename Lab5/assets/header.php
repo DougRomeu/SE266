3978848f1e22a3ed07dc5e1b3519d8c1c34d67c9
Doug Rose <drrose@email.neit.edu>
2017-11-08 13:39:24 -0500

    basic setup

---
+++ Lab5/assets/header.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Doug
+ * Date: 10/16/2017
+ * Time: 12:01 PM
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Lab 5 Sites</title>
+    <!-- css goes here -->
+    <style>
+        table {border:1px solid black; text-align: center; position: relative; margin: auto;}
+        td {border:1px solid black; color: darkred;}
+        th {border:1px solid black; color: red;}
+        h1 {color: darkred; margin:auto;}
+        h3 {color: darkred; margin:auto;}
+        a {color: red;}
+        a:hover {color: darkred;}
+        body {text-align: center;}
+        #wrapper {border: 3px solid darkred; height: auto; width: 50%; position: relative; margin: auto; padding-bottom: 100px;}
+        form {padding-top: 25px; text-align: center}
+        nav {background-color: white; border: 3px solid darkred; text-align: center; width: 50%; position: relative; margin: auto; padding: 7px 0;}
+    </style>
+    <!-- javaScript goes here -->
+</head>
+<body>
+    <header>
+    </header>
+    <div id="wrapper">
+<section>
---
+++ Lab5/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Doug
+ * Date: 11/8/2017
+ * Time: 1:36 PM
+ */
+require_once ('assets/dbconn.php');
+
+
+include_once ('assets/header.php');
+
+include_once ('assets/footer.php');
