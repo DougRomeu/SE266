c4bc17ecb426ca0fe055942b8bf8d26f4508a1ee
Doug Rose <drrose@email.neit.edu>
2017-11-19 16:33:53 -0500

    log in

--- Test/assests/header.php
+++ Test/assests/header.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <title>Doug Rose</title>
-    <link href="https://fonts.googleapis.com/css?family=Prompt" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Monda" rel="stylesheet">
     <style>
-        body {font-family: 'Prompt', sans-serif;
+        body {font-family: 'Monda', sans-serif;
             height: 100%;
             width: 100%;
             position: relative;}
@@ -13,6 +13,7 @@
             background-image: url('../images/3.jpg');
             background-repeat: no-repeat;
             background-size: 100%;
+            position: fixed;
             height: 100%;
             width: 100%;
         }
@@ -20,7 +21,7 @@
             height: 140px;
             width: 100%;}
         #loginBox {
-            background-color: #bf0000;
+            background-color: #fafafa;
             width: 320px;
             height: 440px;
             margin: auto;
@@ -40,16 +41,17 @@
         #usernameBox {
             margin: auto;
             text-align: center;
-            color: white;}
+            color: #7d0000;}
         #passwordBox {
             margin: auto;
             text-align: center;
-            color: white;}
+            color: #7d0000;}
         #buttonBox {
             width:auto;
             text-align: center;
             margin-top: 40px;}
         #btnLogin {
+            font-family: 'Monda', sans-serif;
             text-align: center;
             height: 50px;
             width: 200px;
@@ -69,41 +71,62 @@
         #btnLogin:active {
             background-color: white;
             box-shadow: 0 3px 4px 0 rgba(0,0,0,0.24), 0 4px 12px 0 rgba(0,0,0,0.19);;
-            transform: translateY(4px);
-        }
+            transform: translateY(4px);}
+        #btnLogin:focus {
+            outline: none;
+            background-color: white;
+            color: #7d0000;
+            border: 2px solid #7d0000;
+            transition-duration: 0.4s;
+            box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);}
         #username {
             height: 25px;
             width: 190px;
-            border-radius: 4px;
+            border-radius: 8px;
             padding-left: 10px;
-            border: 0;}
+            border: 1px solid #7d0000;}
+        #username:focus {
+            outline: none;}
         #password {
             height: 25px;
             width: 190px;
-            border-radius: 4px;
+            border-radius: 8px;
             padding-left: 10px;
-            border: 0;}
-        h3 {margin-bottom: 0;}
+            border: 1px solid #7d0000;}
+        #password:focus {
+            outline: none;}
+        h3 {
+            margin-bottom: 0;}
         #forgotPasswordBox{
             width: 200px;
             height: 40px;
-            color: white;
+            color: #7d0000;
             padding-top: 10px;
             text-align: center;}
         #fgtPass {
             text-decoration: none;
-            color:white;}
-        #fgtPass:hover {color: #7d0000;
+            color:#7d0000;}
+        #fgtPass:hover {
+            color: #bf0000;
+            font-weight: bold;}
+        #fgtPass:focus {
+            outline: none;
+            color: #bf0000;
             font-weight: bold;}
         #registerBox{
             width: 200px;
             height: 50px;
-            color: white;
+            color: #7d0000;
             text-align: center;}
         #register {
             text-decoration: none;
-            color:white;}
-        #register:hover {color: #7d0000;
+            color:#7d0000;}
+        #register:hover {
+            color: #bf0000;
+            font-weight: bold;}
+        #register:focus {
+            outline: none;
+            color: #bf0000;
             font-weight: bold;}
     </style>
 </head>
