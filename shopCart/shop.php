f17fb3af3354d1ecd7d8df1467e84af31c0fa6ad
Doug Rose <drrose@email.neit.edu>
2017-12-06 14:32:43 -0500

    why

--- shopCart/shop.php
+++ shopCart/shop.php
@@ -11,46 +11,41 @@ include_once('assets/header.php');
 
 ?>
 
-<h1>SHOPPING PAGE</h1>
+    <h1>SHOPPING PAGE</h1>
 <?php echo(getCategoryNames($db))?><input type="submit" name="action" value="Search">
 
 <?php
 echo(displayProducts($db, $category));
 ?>
-<br />
-<br />
-<h1>CART</h1>
+    <br />
+    <br />
+    <h1>CART</h1>
 <?php
-    $total = 0;
-    if(isset($_SESSION['cart']) || count($_SESSION['cart']) > 0){
-        foreach($_SESSION['cart'] as $index => $value){
-            $price = displayProduct($db, $value, $index);
+$total = 0;
+if(isset($_SESSION['cart']) || count($_SESSION['cart']) > 0){
+    foreach($_SESSION['cart'] as $index => $value){
+        $price = displayProduct($db, $value, $index);
 
-            if($price != -1){
-                $total += $price;
-                $_SESSION['total'] = $total;
-            }
+        if($price != -1){
+            $total += $price;
+            $_SESSION['total'] = $total;
         }
-
-        echo "<br /><br />Total: $  $total";
-    }else{
-        echo "<h4>Cart is currently empty...</h4>";
     }
-    ?>
-<br />
+
+    echo "<br /><br />Total: $  $total";
+}else{
+    echo "<h4>Cart is currently empty...</h4>";
+}
+?>
+    <br />
     <form action="checkout.php">
         <input type="submit" id="btnCheckout" name="checkout" value="Checkout">
     </form>
     <br />
     <br />
-<<<<<<< HEAD
-    <!--<input type="submit" name="action" value="Clear Cart"/>-->
-
-=======
-<form method="POST" action="#">
-    <input type="submit" name="action" value="Clear Cart"/>
-</form>
->>>>>>> shopCart
+    <form method="POST" action="#">
+        <input type="submit" name="action" value="Clear Cart"/>
+    </form>
 <?php
 include_once('assets/footer.php')
 ?>
